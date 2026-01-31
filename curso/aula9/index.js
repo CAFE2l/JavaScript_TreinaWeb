15adbbb518c2c53cc5e249a0e92824a84f0f370c
CAFÉ <pciskolargx@gmail.com>
2026-01-31 13:02:30 -0300

    generation function

---
+++ curso/aula9/index.js
@@ -0,0 +1,14 @@
+// Função Geradora (ou generator function)
+function* gerador(){
+    let i = 0;
+    while(i < 5){
+        yield ++i;
+    }
+}
+
+
+const g = gerador();
+
+for (let index = 0; index < 10; index++){
+    console.log(g.next());
+}
