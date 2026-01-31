a42fa6a5d90042f6e42d817b08920aace11f92a4
CAFÉ <pciskolargx@gmail.com>
2026-01-31 10:04:17 -0300

    learning functions

---
+++ index.js
@@ -0,0 +1,47 @@
+// Pârametros posicional
+
+// function calculador(valorX, valorY){
+//     console.log(valorX + valorY);
+// }
+
+// calculador(10,20);
+
+// Pârametros com valor default
+// function calculador(valorX, valorX){
+//     console.log(valorX + valorX);
+// }
+
+// calculador(10, 25);
+
+// Atribuição por desetruturação 
+
+// const { nome, idade } = {
+//     nome: 'ariel', 
+//     idade: 25,
+// }
+
+// console.log(nome, idade);
+
+// const [nome, idade] = ["Ariel", 29];
+
+// function printUser([nome, idade]){
+//     console.log(nome, idade);
+// }
+
+// printUser(["Ariel", 29]);
+
+// atribuição por rest operator
+
+// function printUser({nome, idade, ...props}){
+//     console.log(nome, idade, props);
+// }
+
+// function printUser(){
+//     console.log(arguments);
+// }
+// printUser({
+//     nome: 'Ariel',
+//     idade: 52,
+//     cor: 'Preto',
+//     sexo: 'M',
+// }, 'segundo argumento');
